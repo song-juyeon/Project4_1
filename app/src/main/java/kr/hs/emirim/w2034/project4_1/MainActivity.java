71b4e292532cc0931c1a8e84a2d9c19d215b13a3
송주연 <w2034@e-mirim.hs.kr>
2021-04-08 19:52:52 +0900

    Update Error Toast Message

--- app/src/main/java/kr/hs/emirim/w2034/project4_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2034/project4_1/MainActivity.java
@@ -3,11 +3,13 @@ package kr.hs.emirim.w2034.project4_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             String strEdit1 = edit1.getText().toString();
             String strEdit2 = edit2.getText().toString();
+            Log.v("***strEdit1", strEdit1);
+            if(strEdit1.equals("") || strEdit2.equals("")){
+                Toast.makeText(getApplicationContext(), "계산에 필요한 값을 입력하지 않았습니다.", Toast.LENGTH_SHORT).show();
+                return;
+            }
             int num1 = Integer.parseInt(strEdit1);
             int num2 = Integer.parseInt(strEdit2);
             int result = 0;
